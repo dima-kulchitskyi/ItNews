61701627508db8e2b0305c573ac7eb64ee15e1f2
Dima Kulchitskyi <prodigy3452@gmail.com>
2017-12-16 18:15:38 +0200

    init

---
+++ ItNews.Business/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using ItNews.Business.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItNews.Business.Entities
+{
+    public class AppUser : IEntity
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Password { get; set; }
+    }
+}
+
---
+++ ItNews.Business/Entities/Article.cs
@@ -0,0 +1,19 @@
+﻿using ItNews.Business.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItNews.Business.Entities
+{
+    public class Article : IEntity
+    {
+        public virtual Guid Id { get; set; }
+        public virtual AppUser Author { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Image { get; set; }
+        public virtual string Content { get; set; }
+        public virtual DateTime Date { get; set; }
+    }
+}
