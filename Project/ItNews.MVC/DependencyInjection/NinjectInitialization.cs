979e48751e485cbee213f941db8da3f94048c320
Dima Kulchitskyi <prodigy3452@gmail.com>
2018-01-05 00:57:22 +0200

    install unity

--- Project/ItNews.MVC/DependencyInjection/NinjectInitialization.cs
+++ Project/ItNews.MVC/DependencyInjection/NinjectInitialization.cs
@@ -7,17 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Unity;
+using Unity.AspNet.Mvc;
 
 namespace ItNews.Mvc.DependencyInjection
 {
     public static class NinjectInitialization
     {
         public static void Initialize(INinjectModule module)
-        {  
+        {
+            var container = new UnityContainer();
+
             var kernel = new StandardKernel(module);
             kernel.Unbind<ModelValidatorProvider>();
 
-            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
 }
