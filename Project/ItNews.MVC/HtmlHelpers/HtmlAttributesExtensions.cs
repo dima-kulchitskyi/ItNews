b82ce5e513a06c03dc3c0004a0dbf0611326e22e
Dima Kulchitskyi <prodigy3452@gmail.com>
2018-01-11 03:51:35 +0200

    add HtmlAttributes Helper(object/dictionary -> html atrr's string)

---
+++ Project/ItNews.MVC/HtmlHelpers/HtmlAttributesExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ItNews.Mvc.HtmlHelpers
+{
+    public static class HtmlAttributesExtensions
+    {
+        private static string FromDictionary(IDictionary<string, object> htmlAttributes)
+        {
+            if (htmlAttributes == null || htmlAttributes.Count == 0)
+                return string.Empty;
+
+            var sb = new StringBuilder();
+
+            foreach (var pair in htmlAttributes)
+                sb.Append(pair.Key).Append("=\"").Append(pair.Value).Append("\" ");
+
+            return sb.ToString();
+        }
+
+        public static HtmlString HtmlAttributes(this HtmlHelper helper, object htmlAttributes)
+        {
+            if (htmlAttributes == null)
+                return new HtmlString(string.Empty);
+
+            return new HtmlString(FromDictionary(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes)));
+        }
+
+        public static HtmlString HtmlAttributes(this HtmlHelper helper, Dictionary<string, object> htmlAttributes)
+        {
+            return new HtmlString(FromDictionary(htmlAttributes));
+        }
+    }
+}
